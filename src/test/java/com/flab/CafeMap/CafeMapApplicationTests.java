67163873cf20e7c8467eec8c455bfa83bfbe0cd7
dilmah0203 <dilmah0203@naver.com>
2022-08-24 01:21:47 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/flab/CafeMap/CafeMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.CafeMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
