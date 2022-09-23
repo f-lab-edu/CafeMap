412071aa5896181eb51c02721f6e21e431ca7454
dilmah0203 <dilmah0203@naver.com>
2022-09-24 03:00:16 +0900

    MyBatis 매핑 xml 파일 생성 및 SQL작성

--- src/main/java/com/flab/CafeMap/domain/user/User.java
+++ src/main/java/com/flab/CafeMap/domain/user/User.java
@@ -1,12 +1,15 @@
 package com.flab.CafeMap.domain.user;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter //getter 메소드를 자동으로 생성해주는 애노테이션
-@Setter //setter 메소드를 자동으로 생성해주는 애노테이션
-@AllArgsConstructor //모든 인자가 존재하는 생성자를 만들어주는 애노테이션
+@Getter //getter 메소드를 자동으로 생성
+@Setter //setter 메소드를 자동으로 생성
+@NoArgsConstructor //인자가 없는 생성자를 만들어준다.
+@AllArgsConstructor //모든 인자가 존재하는 생성자를 만들어준다.
 public class User {
 
     private Long id;
@@ -14,7 +17,6 @@ public class User {
     private String name;
     private String password;
     private String phoneNumber;
-    private String createdAt;
-    private String modifiedAt;
-
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 }
--- src/main/java/com/flab/CafeMap/domain/user/dao/UserMapper.java
+++ src/main/java/com/flab/CafeMap/domain/user/dao/UserMapper.java
@@ -1,5 +1,14 @@
 package com.flab.CafeMap.domain.user.dao;
 
-public class UserMapper {
+import com.flab.CafeMap.domain.user.User;
+import java.util.Optional;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
+public interface UserMapper {
+
+    void insertUser(User user);
+
+    int updateUser(User user);
+    Optional<User> findById(Long id);
 }
