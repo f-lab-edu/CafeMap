e4f631675288ddbabe166808a137037508f0561d
dilmah0203 <dilmah0203@naver.com>
2022-10-16 04:53:57 +0900

    DDL 파일 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `user`
 (
-    `id`           int         NOT NULL AUTO_INCREMENT,
-    `login_id`     varchar(50) NOT NULL,
-    `name`         varchar(50) NOT NULL,
-    `password`     varchar(50) NOT NULL,
-    `phone_number` varchar(50) NOT NULL,
-    `created_at`   timestamp   NOT NULL,
-    `modified_at`  timestamp NULL DEFAULT NULL,
-    `created_by`   varchar(50) NOT NULL,
-    `modified_by`  varchar(50) DEFAULT NULL,
+    id           int         NOT NULL AUTO_INCREMENT,
+    login_id     varchar(50) NOT NULL,
+    name         varchar(50) NOT NULL,
+    password     varchar(50) NOT NULL,
+    phone_number varchar(50) NOT NULL,
+    created_at   timestamp   NOT NULL,
+    modified_at  timestamp NULL DEFAULT NULL,
+    created_by   varchar(50) NOT NULL,
+    modified_by  varchar(50) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
