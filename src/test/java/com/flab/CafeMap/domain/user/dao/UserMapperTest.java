04e11c4683a0f8bbd829c42c8af655afb5517ebf
dilmah0203 <dilmah0203@naver.com>
2022-10-17 22:28:29 +0900

    @Profile 애노테이션 추가

--- src/test/java/com/flab/CafeMap/domain/user/dao/UserMapperTest.java
+++ src/test/java/com/flab/CafeMap/domain/user/dao/UserMapperTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @SpringBootTest
+@Profile("test")
 class UserMapperTest {
 
     @Autowired
