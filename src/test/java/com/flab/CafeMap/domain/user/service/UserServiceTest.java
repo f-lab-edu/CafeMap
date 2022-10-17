92984e603e67f233e5fa0ab451a401b5dc1cbfba
dilmah0203 <dilmah0203@naver.com>
2022-10-18 01:13:48 +0900

    @ActiveProfile으로 변경 및 schema.sql 경로 변경

--- src/test/java/com/flab/CafeMap/domain/user/service/UserServiceTest.java
+++ src/test/java/com/flab/CafeMap/domain/user/service/UserServiceTest.java
@@ -8,12 +8,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @SpringBootTest
-@Profile("test")
+@ActiveProfiles("test")
 class UserServiceTest {
 
     @Autowired
--- src/test/java/com/flab/CafeMap/web/user/UserControllerTest.java
+++ src/test/java/com/flab/CafeMap/web/user/UserControllerTest.java
@@ -14,12 +14,13 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class UserControllerTest {
 
     @Mock
