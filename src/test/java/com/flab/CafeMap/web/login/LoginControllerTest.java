fdf042ea3c8db7092a8c6afd5f31e1c599545c9e
dilmah0203 <dilmah0203@naver.com>
2022-11-03 02:52:38 +0900

    UserController 테스트 코드 수정

--- src/test/java/com/flab/CafeMap/web/login/LoginControllerTest.java
+++ src/test/java/com/flab/CafeMap/web/login/LoginControllerTest.java
@@ -1,25 +1,23 @@
 package com.flab.CafeMap.web.login;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flab.CafeMap.domain.login.service.LoginService;
-import com.flab.CafeMap.domain.user.dao.UserMapper;
-import com.flab.CafeMap.domain.user.service.UserService;
+import com.flab.CafeMap.domain.user.User;
 import com.flab.CafeMap.web.login.dto.LoginRequest;
-import com.flab.CafeMap.web.user.dto.UserSaveRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 
 @WebMvcTest(LoginController.class)
@@ -35,19 +33,39 @@ class LoginControllerTest {
     ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    @DisplayName("로그인 성공 시 201 상태코드 반환")
+    @DisplayName("로그인 성공 시 200 상태코드 반환")
     void login() throws Exception {
         //given
         LoginRequest loginRequest = createLoginRequest();
+        User user = User.builder()
+            .loginId("testLoginId")
+            .name("testName")
+            .build();
+        Mockito.when(loginService.login(any(), any())).thenReturn(user);
 
         //when
         mockMvc.perform(post("/login")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(loginRequest))
-            .accept(MediaType.APPLICATION_JSON))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(loginRequest))
+                .accept(MediaType.APPLICATION_JSON))
             //then
-            .andDo(print()).andExpect(status().isCreated());
+            .andDo(print()).andExpect(status().isOk());
     }
+
+    @Test
+    @DisplayName("로그아웃 성공 시 200 상태코드 반환")
+    void logout() throws Exception {
+        //given
+        Mockito.doNothing().when(loginService).logout(any());
+
+        //when
+        mockMvc.perform(post("/logout")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+            //then
+            .andDo(print()).andExpect(status().isOk());
+    }
+
 
     private LoginRequest createLoginRequest() {
         return LoginRequest.builder()
@@ -55,4 +73,5 @@ class LoginControllerTest {
             .password("testPassword")
             .build();
     }
+
 }
