f70353b1f3fdd208147758d8fe175d9699779391
dilmah0203 <dilmah0203@naver.com>
2022-11-08 23:19:58 +0900

    로그인 인증 인터셉터 구현

---
+++ src/main/java/com/flab/CafeMap/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package com.flab.CafeMap.web.interceptor;
+
+import static com.flab.CafeMap.domain.login.service.LoginService.LOGIN_SESSION;
+
+import com.flab.CafeMap.domain.login.exception.UnauthenticatedUserException;
+import javax.servlet.UnavailableException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+
+        log.info("로그인 인증 체크 인터셉터 실행");
+
+        HttpSession httpSession = request.getSession();
+
+        if (httpSession == null || httpSession.getAttribute(LOGIN_SESSION) == null) {
+            log.info("인증되지 않은 사용자 요청");
+            throw new UnauthenticatedUserException();
+        }
+        return true;
+    }
+}
