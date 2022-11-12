4dd0dacbd7b5940150c1d75394eb4c924b9a74fd
dilmah0203 <dilmah0203@naver.com>
2022-11-12 18:47:53 +0900

    불필요한 import문 삭제

--- src/main/java/com/flab/CafeMap/web/user/dto/UserPatchRequest.java
+++ src/main/java/com/flab/CafeMap/web/user/dto/UserPatchRequest.java
@@ -1,6 +1,5 @@
 package com.flab.CafeMap.web.user.dto;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
