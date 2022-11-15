3be910933abc2a00e434fe0924e0db014eb671e4
dilmah0203 <dilmah0203@naver.com>
2022-11-16 00:24:10 +0900

    UserAddressService 로직 추가

---
+++ src/main/java/com/flab/CafeMap/domain/user/service/UserAddressService.java
@@ -0,0 +1,44 @@
+package com.flab.CafeMap.domain.user.service;
+
+import com.flab.CafeMap.domain.login.exception.UserNotFoundException;
+import com.flab.CafeMap.domain.user.User;
+import com.flab.CafeMap.domain.user.UserAddress;
+import com.flab.CafeMap.domain.user.dao.UserAddressMapper;
+import com.flab.CafeMap.domain.user.dao.UserMapper;
+import com.flab.CafeMap.web.user.dto.UserAddressSaveRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @RequiredArgsConstructor : final 필드에 대해 생성자 생성
+ * @Transactional : 선언적 트랜잭션에 사용되는 애노테이션으로 테스트 이후 롤백
+ */
+
+@Service
+@RequiredArgsConstructor
+public class UserAddressService {
+
+    private final UserMapper userMapper;
+
+    private final UserAddressMapper userAddressMapper;
+
+    @Transactional
+    public UserAddress addUserAddress(Long id, UserAddressSaveRequest userAddressSaveRequest) {
+        UserAddress userAddress = createUserAddress(id, userAddressSaveRequest);
+        userAddressMapper.insertUserAddress(userAddress);
+        return userAddress;
+    }
+
+    private UserAddress createUserAddress(Long id, UserAddressSaveRequest userAddressSaveRequest) {
+        String loginId = findUserLoginId(id);
+        return null;
+    }
+
+    private String findUserLoginId(Long id) {
+        return userMapper.selectUserById(id).orElseThrow(() -> {
+            throw new UserNotFoundException();
+        }).getLoginId();
+    }
+
+}
