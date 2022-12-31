292514cd2ac5384debbda232120a226ff0ade7c0
dilmah0203 <dilmah0203@naver.com>
2022-12-31 14:10:14 +0900

    어노테이션 주석 수정

--- src/main/java/com/flab/CafeMap/domain/user/User.java
+++ src/main/java/com/flab/CafeMap/domain/user/User.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 /**
  * @Getter : getter 메소드를 자동으로 생성
  * @Builder : 빌더 패턴을 사용할 수 있게 도와주는 애노테이션
- * @NoArgsConstructor : 인자가 없는 생성자를 만들어준다.
- * @AllArgsConstructor  : 빌더 패턴을 사용할 수 있게 도와주는 애노테이션
+ * @NoArgsConstructor : 필드가 없는 기본 생성자를 만들어준다.
+ * @AllArgsConstructor  : 모든 필드를 받는 생성자를 만들어준다.
  */
 
 @Getter
