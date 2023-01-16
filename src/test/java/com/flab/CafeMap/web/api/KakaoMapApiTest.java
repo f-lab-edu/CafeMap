d7337bec5873c10ee65f82de124f518dfb353f23
dilmah0203 <dilmah0203@naver.com>
2023-01-16 19:02:11 +0900

    테스트 어노테이션 주석 설명 추가

--- src/test/java/com/flab/CafeMap/web/api/KakaoMapApiTest.java
+++ src/test/java/com/flab/CafeMap/web/api/KakaoMapApiTest.java
@@ -9,6 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * @SpringBootTest : SpringBoot 통합테스트에 사용되는 애노테이션으로 @SpringBootApplication을 찾아가 하위의 모든 빈을 스캔한다.
+ * @ActiveProfiles : 스프링 컨테이터 실행 시 실행 환경을 지정해주는 애노테이션으로, 테스트 시 특정 빈을 로드
+ */
+
 @SpringBootTest
 @ActiveProfiles("test")
 class KakaoMapApiTest {
