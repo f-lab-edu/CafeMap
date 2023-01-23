8eef39221bb81a272f0fb89706a6d2c14569d34f
dilmah0203 <dilmah0203@naver,com>
2023-01-24 02:42:07 +0900

    불필요한 import문 삭제

--- src/main/java/com/flab/CafeMap/web/api/KakaoMapApi.java
+++ src/main/java/com/flab/CafeMap/web/api/KakaoMapApi.java
@@ -6,7 +6,6 @@ import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
