79d786fc7c1339128fe862629365051551dce873
dilmah0203 <dilmah0203@naver.com>
2023-02-15 20:17:28 +0900

    어노테이션 설명 주석 추가

--- src/main/java/com/flab/CafeMap/domain/user/dao/UserAddressMapper.java
+++ src/main/java/com/flab/CafeMap/domain/user/dao/UserAddressMapper.java
@@ -4,6 +4,10 @@ import com.flab.CafeMap.domain.user.UserAddress;
 import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * @Mapper : MyBatis 매핑 xml의 SQL을 호출하기 위한 인터페이스
+ */
+
 @Mapper
 public interface UserAddressMapper {
 
