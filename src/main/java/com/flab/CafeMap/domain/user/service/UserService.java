b62d16bbee41582c653a29ece8f9737b8b10e218
dilmah0203 <dilmah0203@naver.com>
2023-02-16 02:06:22 +0900

    주석 수정

--- src/main/java/com/flab/CafeMap/domain/user/service/UserService.java
+++ src/main/java/com/flab/CafeMap/domain/user/service/UserService.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @RequiredArgsConstructor : final 필드에 대해 생성자 생성
  * @Transactional : 선언적 트랜잭션에 사용되는 애노테이션으로 테스트 이후 롤백
  */
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
