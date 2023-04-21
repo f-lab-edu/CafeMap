ce96d7557954f2b2b857076abfbce12b3a0055c8
dilmah0203 <dilmah0203@naver.com>
2023-04-21 22:20:59 +0900

    BCrypt를 사용한 PasswordEncoder bean 생성

--- src/main/java/com/flab/CafeMap/web/config/SecurityConfig.java
+++ src/main/java/com/flab/CafeMap/web/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
+
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
