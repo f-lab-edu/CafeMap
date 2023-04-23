c5e3285da2a5b76aae80a1841393109425872d8d
dilmah0203 <dilmah0203@naver.com>
2023-04-23 19:09:28 +0900

    schema.sql pw 길이 변경

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE user
     id           int         NOT NULL AUTO_INCREMENT,
     login_id     varchar(50) NOT NULL,
     name         varchar(50) NOT NULL,
-    password     varchar(50) NOT NULL,
+    password     varchar(100) NOT NULL,
     phone_number varchar(50) NOT NULL,
     created_at   timestamp   NOT NULL,
     modified_at  timestamp NULL DEFAULT NULL,
