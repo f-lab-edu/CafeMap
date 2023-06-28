292dc107ab4639976394312ab6762a33cdf9986f
dilmah0203 <dilmah0203@naver.com>
2023-06-28 22:02:45 +0900

    주석 수정

--- src/main/java/com/flab/CafeMap/web/config/EmbeddedRedisConfig.java
+++ src/main/java/com/flab/CafeMap/web/config/EmbeddedRedisConfig.java
@@ -42,12 +42,16 @@ public class EmbeddedRedisConfig {
         }
     }
 
-    //Embedded Redis가 현재 실행중인지 확인한다.
+    /**
+     * Embedded Redis가 현재 실행중인지 확인한다.
+     */
     private boolean isRedisRunning() throws IOException {
         return isRunning(executeGrepProcessCommand(redisPort));
     }
 
-    //현재 pc와 서버에서 사용가능한 포트를 조회한다.
+    /**
+     * 현재 pc에서 사용가능한 포트를 조회한다.
+     */
     public int findAvailablePort() throws IOException {
         for (int port = 10000; port <= 65535; port++) {
             Process process = executeGrepProcessCommand(port);
@@ -58,14 +62,18 @@ public class EmbeddedRedisConfig {
         throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
     }
 
-    //해당 포트를 사용 중인 프로세스를 확인하는 sh를 실행한다.
+    /**
+     * 해당 포트를 사용 중인 프로세스를 확인하는 sh를 실행한다.
+     */
     private Process executeGrepProcessCommand(int port) throws IOException {
         String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
         String[] shell = {"/bin/sh", "-c", command};
         return Runtime.getRuntime().exec(shell);
     }
 
-    //해당 프로세스가 현재 실행중인지 확인한다.
+    /**
+     * 해당 프로세스가 현재 실행중인지 확인한다.
+     */
     private boolean isRunning(Process process) {
         String line;
         StringBuilder sb = new StringBuilder();
