eb1421c0b31f9519775528962ea639871be867f8
dilmah0203 <dilmah0203@naver.com>
2023-10-31 21:21:21 +0900

    애노테이션 주석 설명 추가

--- src/test/java/com/flab/CafeMap/domain/cafe/dao/CafeMapperTest.java
+++ src/test/java/com/flab/CafeMap/domain/cafe/dao/CafeMapperTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.flab.CafeMap.domain.cafe.Cafe;
 import java.time.LocalDateTime;
-import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
