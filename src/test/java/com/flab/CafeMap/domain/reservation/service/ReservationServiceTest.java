3a3461e0076185a864435e28d4f31a8141b963d3
dilmah0203 <dilmah0203@naver.com>
2023-10-31 17:13:48 +0900

    테스트 코드 수정

--- src/test/java/com/flab/CafeMap/domain/reservation/service/ReservationServiceTest.java
+++ src/test/java/com/flab/CafeMap/domain/reservation/service/ReservationServiceTest.java
@@ -21,9 +21,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class ReservationServiceTest {
 
