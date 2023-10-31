ac6343b97eaf7438dac13854a1e514ab6ca5876b
dilmah0203 <dilmah0203@naver.com>
2023-10-31 14:23:04 +0900

    스키마 변경

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -2,15 +2,15 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user
 (
-    id           int         NOT NULL AUTO_INCREMENT,
-    login_id     varchar(50) NOT NULL,
-    name         varchar(50) NOT NULL,
+    id           int          NOT NULL AUTO_INCREMENT,
+    login_id     varchar(50)  NOT NULL,
+    name         varchar(50)  NOT NULL,
     password     varchar(100) NOT NULL,
-    phone_number varchar(50) NOT NULL,
-    created_at   timestamp   NOT NULL,
-    modified_at  timestamp NULL DEFAULT NULL,
-    created_by   varchar(50) NOT NULL,
-    modified_by  varchar(50) DEFAULT NULL,
+    phone_number varchar(50)  NOT NULL,
+    created_at   timestamp    NOT NULL,
+    modified_at  timestamp    NULL DEFAULT NULL,
+    created_by   varchar(50)  NOT NULL,
+    modified_by  varchar(50)       DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -20,12 +20,12 @@ CREATE TABLE user_address
     login_id       varchar(50) NOT NULL,
     street_address varchar(50) NOT NULL,
     detail_address varchar(50) NOT NULL,
-    latitude       double NOT NULL,
-    longitude      double NOT NULL,
+    latitude       double      NOT NULL,
+    longitude      double      NOT NULL,
     created_at     timestamp   NOT NULL,
-    modified_at    timestamp NULL DEFAULT NULL,
+    modified_at    timestamp   NULL DEFAULT NULL,
     created_by     varchar(50) NOT NULL,
-    modified_by    varchar(50) DEFAULT NULL,
+    modified_by    varchar(50)      DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -34,8 +34,19 @@ CREATE TABLE cafe
     id         int         NOT NULL AUTO_INCREMENT,
     cafe_id    varchar(50) NOT NULL,
     name       varchar(50) NOT NULL,
-    latitude   double NOT NULL,
-    longitude  double NOT NULL,
+    latitude   double      NOT NULL,
+    longitude  double      NOT NULL,
     created_at timestamp   NOT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `reservation`
+(
+    `id`                 int          NOT NULL,
+    `user_id`            int          NOT NULL,
+    `cafe_id`            int          NOT NULL,
+    `address`            varchar(100) NOT NULL,
+    `reservation_status` varchar(45)  NOT NULL,
+    `created_at`         timestamp    NOT NULL,
+    PRIMARY KEY (`id`)
+)
